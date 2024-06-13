9ecbb1eaafe1ca9ca0164d2d599d8150ea529d99
Qayyum <qayyumabdul660@gmail.com>
2024-06-13 21:51:05 +0530

    application with spring boot

--- java-react/src/main/java/com/example/java_react/GreetingController.java
+++ java-react/src/main/java/com/example/java_react/GreetingController.java
@@ -17,3 +17,5 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
+
+
---
+++ java-react/src/main/java/com/example/java_react/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.java_react;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the Home route!";
+    }
+}
